f37fbdb494e7812162167793fffd7f7357a53717
vexhussain45 <hussainxmd2@gmail.com>
2025-03-11 15:11:26 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hussain',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "🦋𝗜𝗧𝗔𝗖𝗛𝗜 ✞︎ 𝗨𝗖𝗛𝗜𝗛𝗔 ♂️",
+  botOwner: '𝗠𝗥_𝗛𝗨𝗦𝗦𝗔𝗜𝗡', // Your name
+  ownerNumber: '8801334089349', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
